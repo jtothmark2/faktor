d3cf7ba8ac4703c3a11c7891e8368a0b634f1fb3
jtothmark2 <72131114+jtothmark2@users.noreply.github.com>
2020-09-30 11:32:45 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿//BigInteger osztály (struktúra) elérése:
+//1. System.Numerics hozzáadása
+//2. using System.Numerics
+using System;
+using System.Numerics;
+
+namespace faktoriálisBigInteger
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Számok faktoriálisa 20 és 40 között");
+            for (int szam = 20; szam <= 100000; szam++)
+            {
+                BigInteger faktor = 1;
+                for (int i = 2; i <= szam; i++)
+                {
+                    faktor = faktor * i;
+                }
+                Console.WriteLine($"{szam}!={faktor}");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("faktoriálisBigInteger")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("faktoriálisBigInteger")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("b715f583-1cd1-43ca-a262-1134ca5334a7")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
